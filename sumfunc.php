df6857442010294ffacd0d6fe2688637b50e770e
Saheed Mohammed <86488517+SMDCreative@users.noreply.github.com>
2021-06-25 19:06:19 +0100

    Create sumfunc.php

---
+++ sumfunc.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+
+This is sum function in PHP
+Task given by SideHustle
+
+Write a sum function that takes an array of numbers and returns the sum of these numbers.
+
+**/
+
+function sum($saheed1, $saheed2) {
+	return $saheed1 + $saheed2 ."<br>";
+}
+
+echo sum(50,70);
+
+?>
